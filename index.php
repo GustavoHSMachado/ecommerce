a8660e44c95339e9cfefabc9320e33a0a35f734f
unknown <GustavoHSMachado>
2019-04-10 14:33:40 -0300

    tela de logim

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php 
-
+	
+	session_start(); //Para usar o login tem que usar sessão, aqui estou iniciando a sessão.
+		
 	require_once("vendor/autoload.php"); // o composer e trazer suas dependencias.
-
-	/* OS "namespaces"*/
+	//require_once("functions.php");
+	
+	/* OS "namespaces" */
 	use \Slim\Slim; 
 	use \Hcode\Page;
 	use \Hcode\PageAdmin;
+	use Hcode\Model\User;
 
 	$app = new Slim(); // començando as rotas.
 
@@ -22,12 +26,43 @@
 	});
 	
 	$app->get('/admin', function() { //chmando as rotas para chamar a pagina do administrador
-	
+		
+		User::verifyLogin(); // usando método criado na Classe User para verificar se o usuario logou mesmo. 
+		
 		$page = new PageAdmin(); // carregar o header
 
 		$page->setTpl("index"); // chama o template.
 		
 		// depois que esse metodo é usado o destruct executa automaticamente e finaliza o codigo.
+
+	});
+	
+	$app->get('/admin/login', function() {
+    
+		$page = new PageAdmin([ //Array Para Desabilitar header e o foooter padrão pois a pagina de login não precisa nesse caso.
+			"header"=>false,
+			"footer"=>false
+		]);
+
+		$page->setTpl("login");
+
+	});
+	
+	$app->post('/admin/login', function() {
+
+		User::login($_POST["deslogin"], $_POST["despassword"]); //chamando o metodo para conferir se o login e senha existe, criado ma User.php.
+
+		header("Location: /admin"); //se ele existir ele manda para pagina admin.
+		exit;
+
+	});
+
+	$app->get('/admin/logout', function() {
+
+		User::logout(); //Usando constante para chamar e usar o metodo de logout criado na User.php.
+
+		header("Location: /admin/login"); //Se der certo ele manda para a pagina de login do administrador.
+		exit;
 
 	});
 
