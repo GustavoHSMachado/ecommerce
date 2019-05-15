e2de2d15971be9d960a845e271d6f41da5e2073d
unknown <GustavoHSMachado>
2019-05-15 20:52:30 -0300

    Admin - Categorias

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -34,7 +34,7 @@
 				switch ($method) {
 
 					case "get":
-						return $this->values[$fieldName];
+						return (isset ($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
 					break;
 
 					case "set":
