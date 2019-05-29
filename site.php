d74fb082541300cf8f1a6450b4bcd7f43a104078
unknown <GustavoHSMachado>
2019-05-29 14:27:14 -0300

    Categorias VS Produtos

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 	/* OS "namespaces" */
 	use \Hcode\Page;
 	use \Hcode\Model\Product;
+	use \Hcode\Model\Category;
 	
 	$app->get('/', function() { //chmando as rotas 
 		
@@ -16,6 +17,21 @@
 		
 		// depois que esse metodo é usado o destruct executa automaticamente e finaliza o codigo.
 
+	});
+	
+	$app -> get('/categories/:idcategory', function($idcategory){
+		
+		$category = new Category();
+		
+		$category -> get((int)$idcategory);
+		
+		$page = new Page();
+
+		$page->setTpl("category", [
+			'category' => $category -> getValues(),
+			'products' => Product::checkList($category -> getProducts())
+		]);
+		
 	});
 
 ?>
